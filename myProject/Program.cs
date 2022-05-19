170b1cd60b71ef9e62226e23caceaa3d246f1df6
MishaEgorov <86955774+MishaEgorov@users.noreply.github.com>
2022-05-19 23:59:55 +0300

    Revert "LINQ"

--- myProject/Program.cs
+++ myProject/Program.cs
@@ -1,96 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace myProject
 {
     enum  Catalog {apple, banana, tomato, pear };
     class Program
     {
+ 
         static void Main(string[] args)
         {
-            List<Products> products = new List<Products>()
-            {
-                 new Fruit(1, Catalog.apple, 10, 11.5f, 100, 1),
-                 new Fruit(2, Catalog.banana, 5, 6.1f, 50, 2),
-                 new Fruit(3, Catalog.pear, 10, 10.2f, 156.5m, 2),
-                 new Vegetables(4, Catalog.tomato, 6, 2, 15.1m, 3)
-            };
+            List<Products> products = new List<Products>();
 
-            List<Supplier> suppliers = new List<Supplier>()
-            {
-                 new Supplier(1, "Магнит", "Тула"),
-                 new Supplier(2, "Спар", "Москва"),
-                 new Supplier(3, "Пятёрочка", "Тула"),
-            };
+            Fruit apple = new Fruit(Catalog.apple,1,10,11.5f,100);
+            Fruit banana = new Fruit(Catalog.banana, 2, 5,6.1f,50);
+            Fruit pear = new Fruit(Catalog.pear, 4, 10, 10.2f,156.5m);
 
-            List<Products> GetProducts() 
+            Vegetables tomato = new Vegetables(Catalog.tomato, 3, 6, 2, 15.1m);
+
+            products.Add(apple);
+            products.Add(banana);
+            products.Add(tomato);
+            products.Add(pear);
+
+            foreach (Products pr in products)
             {
-                return products;
+                Console.WriteLine(pr.Print());
             }
 
-            List<Supplier> GetSupplier() 
-            {
-                return suppliers;
-            }
-
-            var result =
-                from allProducts in GetProducts()
-                join allSuppler in GetSupplier()
-                on allProducts.supplerId equals allSuppler.id into productsSuplerGroup
-                from subSuppler in productsSuplerGroup.DefaultIfEmpty()
-                select new { productsName = allProducts.catalog, supplerName = subSuppler?.name };
-
-            foreach (var curr in result) 
-            Console.WriteLine($"{curr.productsName} от поставщика {curr.supplerName}");
-
+            Console.WriteLine();
             SortList sortList = new SortList();
-
-            Console.WriteLine("Сортировка по цене: ");
             products.Sort(sortList.ComparePrise);
-            foreach (var curr in products) Console.WriteLine(curr.Print());
 
-            Console.WriteLine("Сортировка по количеству: ");
+            foreach (Products pr in products)
+            {           
+                Console.WriteLine(pr.Print());
+            }
+            Console.WriteLine();
             products.Sort(sortList.CompareAmount);
-            foreach (var curr in products) Console.WriteLine(curr.Print());
+
+            foreach (Products pr in products)
+            {
+                Console.WriteLine(pr.Print());
+            }
+
         }
     }
     class Products
     {
-        public int id;
         public Catalog catalog;
+        public int id;
         public int amount;
         public decimal price;
         protected string str;
-        public int supplerId;
-
-        public Products(int id, Catalog catalog, int amount,decimal price, int supplerId)
+       
+        public Products(Catalog catalog,int id, int amount,decimal price)
         {
-            this.id = id;
             this.catalog = catalog;
+            this.id = id;
             this.amount = amount;
             this.price = price;
-            this.supplerId = supplerId;
         }
-
         public Products() { }
 
         public virtual string Print()
         {
             str = "Название продукта:" + catalog +
-                   " ID:" + id + " Количество товара:" + amount + " Цена:"+price;
+                            " ID:" + id + " Количество товара:" + amount + " Цена:"+price;
             return str;
         }
     }
     class Fruit:Products
     {
         public float kg;
-
-        public Fruit(int id, Catalog catalog, int amount,float kg,decimal price, int supplerId) : base (id, catalog, amount, price, supplerId)
+        public Fruit(Catalog catalog, int id, int amount,float kg,decimal price) : base(catalog,id,amount,price)
         {
             this.kg = kg;
         }
-
         public override string Print()
         {
             base.Print();
@@ -100,12 +85,10 @@ namespace myProject
     class Vegetables:Products
     {
         public int pcs;
-
-        public Vegetables(int id, Catalog catalog, int amount, int pcs, decimal price, int supplerId) : base(id, catalog,  amount,price, supplerId)
+        public Vegetables(Catalog catalog, int id, int amount, int pcs, decimal price) : base(catalog, id, amount,price)
         {
             this.pcs = pcs;
         }
-
         public override string Print()
         {
             base.Print();
@@ -120,7 +103,6 @@ namespace myProject
             else if (obj1.price < obj2.price) return -1;
             return 0;
         }
-
         public int CompareAmount(Products obj1, Products obj2)
         {
             if (obj1.amount > obj2.amount) return 1;
@@ -128,19 +110,6 @@ namespace myProject
             return 0;
         }
 
-    }
-    class Supplier
-    {
-        public int id;
-        public string name;
-        public string city;
-
-        public Supplier(int id,string name,string city)
-        {
-            this.id = id;
-            this.name = name;
-            this.city = city;
-        }
     }
     public interface IComparePrise <in T>
     {
